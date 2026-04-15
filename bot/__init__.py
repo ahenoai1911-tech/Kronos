a316d7b3d2f7d136ba9c6cdb3730f90f967ded41
ahenoai1911-tech <ahenoai1911@gmail.com>
2026-04-15 09:23:11 -0400

    Add files via upload

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,4 +1,4 @@
-from .kb import (
+from .keyboards.kb import (
     get_main_menu, get_tasks_menu, get_task_actions,
     get_priority_keyboard, get_category_keyboard,
     get_plan_menu, get_settings_menu, get_back_button
